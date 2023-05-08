f94b694cd57386da13401f185258c8f1cdf3f621
Clément Muth <clementmuth@gmail.com>
2023-05-08 16:58:24 +0200

    fix: remove broken fetch

--- app/[userId]/page.tsx
+++ app/[userId]/page.tsx
@@ -1,7 +1,6 @@
 import { notFound } from "next/navigation";
 
 export default async function Home({ params: { userId } }: any) {
-  fetch("", { cache: "force-cache" });
   if (userId !== "romanie" && userId !== "clement") return notFound();
 
   return (
